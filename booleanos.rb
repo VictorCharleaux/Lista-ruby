5eb5a333d1920d6e40ed7f97310a0e49dd43ba25
Victor Charleaux <victorcharleaux@outlook.com>
2025-06-08 03:44:34 -0300

    Booleanos

---
+++ booleanos.rb
@@ -0,0 +1,21 @@
+=begin
+AND (e) &&
+Verdade e Verdade = Verdade
+Verdade e Falso = Falso
+Falso e Verdade = Falso
+Falso e Falso = Falso
+
+OR (ou) ||
+Verdade e Verdade = Verdade
+Verdade e Falso = Verdade
+Falso e Verdade = Verdade
+Falso e Falso = Falso
+
+NOT (não) !
+Não verdade = Falso
+Não Falso = Verdade
+=end
+
+puts false && true
+puts false || true 
+puts !false && true 
